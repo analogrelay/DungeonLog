5a6f398c28981c53c5a6414df47aa71a612b3c12
Andrew Nurse <andrew@andrewnurse.net>
2011-12-09 10:45:07 -0800

    Moved files and added website bin to Git. (Need to find a better way to handle that...)

---
+++ DungeonLog.Web/App_Data/Schema/01-UsersPlayersDMsGames.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Users(
+	Id			int				PRIMARY KEY IDENTITY,
+	UserName	nvarchar(256)	NOT NULL,
+	Email		nvarchar(256)	NOT NULL
+)
+
+CREATE TABLE Games(
+	Id			int				PRIMARY KEY IDENTITY,
+	Name		nvarchar(256)	NOT NULL,
+	OwnerUserId	int				NOT NULL
+)
+
+CREATE TABLE Players(
+	Id			int				PRIMARY KEY IDENTITY,
+	GameId		int				NOT NULL,
+	UserId		int				NULL,
+	Name		nvarchar(256)	NOT NULL
+)
+
+CREATE TABLE DungeonMasters(
+	Id			int				PRIMARY KEY IDENTITY,
+	GameId		int				NOT NULL,
+	UserId		int				NOT NULL
+)
